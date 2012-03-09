babf9df0e0db6ce0537922e0333dc3da19f7b1ca
Kavon Farvardin <kff5027@psu.edu>
2012-03-09 02:58:47 -0500

    looked at the notes more

--- bnet.py
+++ bnet.py
@@ -1,15 +1,11 @@
 class Node
-    def __init__(self, par):
-        self.parent = par
-        self.children = []
-    def addChild(self, x):
-        self.children.append(x)
-    def prob(self):
-        if len(self.children > 0):
-            product = 1.0
-            for child in self.children
-                product = product * child.prob()
-            return product
-        ## probably should multiply something with
-        ## product and return it. and take care of
-        ## case of no children.
+    def __init__(self):
+        self.probTable = [0.0 for i in range(0,26)]
+    ## P(self | parent)
+    def prob(self, parent):
+        return self.probTable[parent]
+
+class Network:
+    def __init__(self):
+        self.level = []
+
