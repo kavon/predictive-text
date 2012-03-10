eccc0bfd884d04b9ebd4b4491cf39b146c1baec1
Kavon Farvardin <kff5027@psu.edu>
2012-03-10 15:18:22 -0500

    observing non-whitespace should be good

--- bayesian_network.py
+++ bayesian_network.py
@@ -11,6 +11,8 @@ class Node:
         self.obs = 0                # observations of this node
         self.children = []          # ordered list of children of this node
         self.childObs = 0           # total number of observations the children have had
+
+        #might remove this
         self.longestPath = None     # child leading to the longest observed string of nodes
 
     # Less than method used by list.sort() to keep nodes sorted.
@@ -23,15 +25,27 @@ class Node:
         self.children.append(kid)
         self.children.sort()
 
+    # returns reference to child matching description, None otherwise
+    def findChild(self, description):
+        for child in self.children:
+            if child.letter == description:
+                return child
+        return None
 
 class Network:
 
     def __init__(self):
-        self.root = []              # start empty, we can't account for every character
-                                    # initially, and even case matters!
         
-        self.observations = []      # stack of observed nodes to simulate recursion
+        """ I don't think we need a root node variable """
+
+        #self.rootNode = Node(None)              # start empty, we can't account for every character
+                                                 # initially, and even case matters!
+        
+        self.observations = []                  # stack of observed nodes to simulate recursion
+        self.observations.append(Node(None))    # keep root node in stack always
+        
         self.currentPrefix = ''
+        
         self.seenWords = RadixTree()
 
 
@@ -41,29 +55,45 @@ class Network:
         # if you find this character in the string of all whitespace characters
         # so, when the char is a whitespace
         if string.find(string.whitespace, char) != -1:
+
+            
+
             # pop everything off the observed stack while updating each node's value
             # and collecting the total probability of that sequence.
             # then insert the word that was observed and its probability into the radix tree
         
         elif char == '\b': #backspace
+            # there is no need to decrement observations of the popped child
+            # because increments are done once the word was completed.
+            # unfortunately if you observe a new char and then observe backspaces over and over
+            # you'll be adding a bunch of 0 observation nodes to the topmost node for no reason
+            # i might add a prune() method to Node which recursively prunes all 0
+            # observation nodes to slim down on memory use if we're using a lot
+            
+            self.observations.pop()
+
+            """ done """
             # pop and discard the value on the stack
 
         else:
+
+            # update current observed prefix for a word
             self.currentPrefix += char
+
+            # grab the top node. if we're starting at the beginning of a new word it would be the root node
+            topNode = self.observations[len(self.observations) - 1] # choose from children of node on top of stack
             
-            if(len(self.observations) == 0) # stack is empty, first letter of a word
-                for node in self.root:      # so let's look for it
-                    if(node.letter == char):
-                        self.observations.append(node)
-                        break
-                
-                if(len(self.observations) == 0): # we haven't seen this char yet
-                    newguy = Node(char)
-                    self.root.append(newguy)     # a sort() isn't needed here because appending 0 observation node
-                    self.observations.append(newguy)
+            result = topNode.findChild(char)
+            
+            # if the node doesn't have this child
+            if(result == None):
+                result = Node(char)
+                topNode.addChild(result)
+            
+            self.observations.append(result)
+                            
 
-
-
+            """ looks done to me """
             # find the child of the topmost node on the stack which has a letter value corresponding to
             # the observed letter. if it is found then push that on the stack, otherwise create a new one
             # and then push it on the stack
